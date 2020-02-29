baa7e42300304bd1960f379d33fd82eeab4d5d9f
Nimor111 <georgi.bojinov@hotmail.com>
2020-02-29 21:59:25 +0000

    Revamp tmux config, add some xmonad key binds

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,12 @@
 import System.IO (hPutStrLn)
 
-import XMonad (xmonad, terminal, borderWidth, mod4Mask, modMask, normalBorderColor, focusedBorderColor, logHook, layoutHook, manageHook, manageHook, (<+>), startupHook)
+import XMonad (xmonad, terminal, borderWidth, mod4Mask, modMask, normalBorderColor, focusedBorderColor, logHook, layoutHook, manageHook, manageHook, (<+>), startupHook, spawn)
 
 import XMonad.Config.Desktop (desktopConfig)
 
 import XMonad.Util.SpawnOnce (spawnOnce)
 import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeysP)
 
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, xmobarPP, xmobarColor, shorten, PP(..))
 import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
@@ -16,9 +17,6 @@ myModMask = mod4Mask
 myBorderWidth = 2
 myNormalBorderColor = "#292d3e"
 myFocusedBorderColor = "#ffb86c"
--- this is to make enough room for xmobar on the screen
-myLayout = avoidStruts $ layoutHook desktopConfig
-myManageHook = manageHook desktopConfig <+> manageDocks
 
 main :: IO ()
 main = do 
@@ -35,9 +33,18 @@ main = do
         , borderWidth = myBorderWidth
         , normalBorderColor  = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
-	, layoutHook = myLayout
-	, manageHook = myManageHook
-        }
+        , layoutHook = myLayout
+        , manageHook = myManageHook
+        } `additionalKeysP` myKeys
 
 myStartupHook = do
-         spawnOnce "feh --bg-scale $HOME/wallpaper.png &"
+        spawnOnce "feh --bg-scale $HOME/wallpaper.png &"
+
+-- this is to make enough room for xmobar on the screen
+myLayout = avoidStruts $ layoutHook desktopConfig
+myManageHook = manageHook desktopConfig <+> manageDocks
+
+myKeys =
+        [ ("M-<Return>", spawn myTerminal)
+        , ("M-d", spawn "dmenu_run -p 'dmenu:'")
+        ]
