e36b0b852251145ba7f1b5fb9ce4bd3309cf4987
Nimor111 <georgi.bojinov@hotmail.com>
2020-07-15 00:04:07 +0300

    Add literate emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,61 +1,20 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Georgi Bojinov"
       user-mail-address "georgi.bojinov@hotmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 (setq doom-font (font-spec :family "JetBrainsMono" :size 18))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+(setq org-startup-indented t
+      org-indent-mode t
+      org-log-done 'note)
+
+(setq org-agenda-files '("~/Nextcloud/org-roam" "~/Nextcloud/Orgzly"))
+
 (setq org-directory "~/Nextcloud/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 
 (setq org-roam-directory "~/Nextcloud/org-roam")
 
-;; set custom mappings for org-roam commands
 (after! org-roam
         (map! :leader
             :prefix "n"
@@ -67,7 +26,8 @@
             :desc "org-roam-insert" "i" #'org-roam-insert
             :desc "org-roam-capture" "c" #'org-roam-capture))
 
-;; make org journal point to roam files and some extra config
+(use-package! org-roam-server)
+
 (use-package! org-journal
       :bind
       ("C-c n j" . org-journal-new-entry)
@@ -77,7 +37,21 @@
       (org-journal-file-format "%Y-%m-%d.org")
       (org-journal-date-format "%A, %d %B %Y"))
 
-;; viewer for org roam files
+(use-package! org-randomnote
+  :after org-randomnote
+  :bind ("C-c r" . org-randomnote))
+
+(load-library "find-lisp")
+(setq org-randomnote-candidates
+      (find-lisp-find-files "~/Nextcloud/org-roam" "\.org$"))
+
+(use-package! ox-reveal
+  :config
+  (setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
+
+(use-package! org-drill
+  :after org)
+
 (use-package! deft
       :after org
       :bind
@@ -88,41 +62,27 @@
       (setq deft-default-extension "org")
       (setq deft-directory "~/Nextcloud/org-roam/"))
 
-;; set org agenda files
-(setq org-agenda-files '("~/Nextcloud/org-roam" "~/Nextcloud/Orgzly"))
+(use-package! rustic
+  :config
+  (setq rustic-format-trigger 'on-save))
 
-;; haskell-mode settings
 (use-package! haskell-mode
       :after haskell-mode
       :custom
       (haskell-tags-on-save t))
 (setq haskell-interactive-popup-error nil)
 
-;; org-random note config
-(use-package! org-randomnote
-  :after org-randomnote
-  :bind ("C-c r" . org-randomnote))
+(add-hook! 'elfeed-search-mode-hook 'elfeed-update)
 
-;; find a random file in the org-roam directory
-(load-library "find-lisp")
-(setq org-randomnote-candidates
-      (find-lisp-find-files "~/Nextcloud/org-roam" "\.org$"))
+(map!
+  (:after dired
+    (:map dired-mode-map
+     "C-x i" #'peep-dired
+     )))
+(evil-define-key 'normal peep-dired-mode-map (kbd "j") 'peep-dired-next-file
+                                             (kbd "k") 'peep-dired-prev-file)
+(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
 
-;; org-reveal settings
-(use-package! ox-reveal
-  :config
-  (setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
-
-;; use org-drill
-(use-package! org-drill
-  :after org)
-
-;; format rust buffers on save
-(use-package! rustic
-  :config
-  (setq rustic-format-trigger 'on-save))
-
-;; easier window navigation
 (map!
  (:after evil
    :en "C-h"   #'evil-window-left
@@ -130,14 +90,4 @@
    :en "C-k"   #'evil-window-up
    :en "C-l"   #'evil-window-right))
 
-;; org-roam-server
-(use-package! org-roam-server)
-;; haskell lsp settings
-;; (use-package! lsp-haskell
-;;   :config
-;;   (setq lsp-haskell-process-path-hie "ghcide")
-;;   (setq lsp-haskell-process-args-hie '())
-;; )
-
-;; automatically update feed when opening elfeed
-(add-hook! 'elfeed-search-mode-hook 'elfeed-update)
+(setq display-line-numbers-type t)
